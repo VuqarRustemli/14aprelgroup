ce4dedb8cd89ded4a6566debc0bf0a1b9b9bc4bb
VuqarRustemli <rustemov1987@gmail.com>
2025-10-27 17:14:01 +0400

    lesson1_task

---
+++ codes/javase/src3/main/Sinif.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Sinif {
+
+	public static void main(String[] args) {
+		System.out.println("Adim: Vuqar");
+		System.out.println("Soyadim: Rustemli");
+		System.out.println("Yasim: 12");
+	}
+
+}
