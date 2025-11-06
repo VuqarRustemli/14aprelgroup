6e7bcc2df776eb6372080421c8db4e70792ed4f3
VuqarRustemli <rustemov1987@gmail.com>
2025-11-06 18:08:34 +0400

    task

---
+++ codes/javase/src10/main/Task3.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Task3 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b (AND): " + (a && b));
+        System.out.println("a || b (OR): " + (a || b));
+        System.out.println("!a (NOT a): " + (!a));
+        System.out.println("!b (NOT b): " + (!b));
+    }
+}
---
+++ codes/javase/src8/main/Task1.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 5;
+
+        int cem = a + b;
+        int ferq = a - b;
+        int hasil = a * b;
+        int qaliq = a % b;
+        int bolme = a / b;
+
+        System.out.println("Cəm: " + cem);
+        System.out.println("Fərq: " + ferq);
+        System.out.println("Hasil: " + hasil);
+        System.out.println("Qalıq: " + qaliq);
+        System.out.println("Bölmə: " + bolme);
+    }
+}
