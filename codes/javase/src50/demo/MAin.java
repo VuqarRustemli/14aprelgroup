b8a9b2d6cc6e432d3da9ad78075123704d366621
VuqarRustemli <rustemov1987@gmail.com>
2026-01-29 19:07:00 +0400

    Task

--- codes/javase/src50/demo/MAin.java
+++ codes/javase/src50/demo/MAin.java
@@ -1,5 +1,7 @@
 package demo;
 
+import demo.School.Student;
+
 public class MAin {
     public static void main(String[] args) {
 
---
+++ codes/javase/src51/main/RuntimeException.java
@@ -0,0 +1,32 @@
+package main;
+
+public class RuntimeException {
+
+	public static void main(String[] args) {
+//		Index out of bound exception
+//		int[] arr = {12, 4, 6, 7};
+//		int a = 0;
+//		a = arr[6];
+//		System.out.println(a);
+//		
+//		
+//	    Arithmetic exception
+//		int b = 12;
+//		int result = b / 0;
+//		System.out.println(result);
+//		
+//		
+//      Null pointer exception
+//		Test t = new Test();
+//		Test t = null;
+//		t.a = 12
+//		System.out.println(t.a);
+//		
+//		
+//		Illegal Argument exception
+//		String s = "123a";
+//		int num = Integer.parseInt(s);
+//		System.out.println(num);
+	}
+
+}
